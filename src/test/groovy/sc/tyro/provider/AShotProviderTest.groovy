7b4255b6828cef87057cebe07fecdeabbd0c7ac9
altus34 <d.avenante@gmail.com>
2021-09-20 10:23:06 -0400

    First import

---
+++ src/test/groovy/sc/tyro/provider/AShotProviderTest.groovy
@@ -0,0 +1,76 @@
+/*
+ * Copyright © 2021 Ovea (d.avenante@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sc.tyro.provider
+
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import sc.tyro.bundle.html5.Img
+import sc.tyro.core.component.Image
+
+import java.nio.file.Path
+
+
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.not
+import static org.hamcrest.io.FileMatchers.anExistingFile
+import static sc.tyro.core.Config.setScreenshotProvider
+import static sc.tyro.core.Tyro.*
+import static sc.tyro.provider.TyroWebTestExtension.BASE_URL
+import static sc.tyro.provider.TyroWebTestExtension.driver
+
+@ExtendWith(TyroWebTestExtension)
+@DisplayName("Screenshot Tests")
+class AShotProviderTest {
+    @BeforeAll
+    static void before() {
+        File screenshotDir = Path.of(System.getProperty("user.dir"), 'target', 'screenshots').toFile()
+        screenshotDir.deleteDir()
+
+        screenshotProvider = new AShotProvider(driver)
+
+        visit BASE_URL + 'index.html'
+    }
+
+    @Test
+    @DisplayName("Should take window screenshot")
+    @Order(1)
+    void windowScreenshot() {
+        File screenshot = new File("target/screenshots/window.png")
+
+        assertThat(screenshot, not(anExistingFile()))
+
+        takeScreenshot('window')
+
+        assertThat(screenshot, anExistingFile())
+    }
+
+    @Test
+    @DisplayName("Should take component screenshot")
+    @Order(2)
+    void componentScreenshot() {
+        Image image = $('img:first') as Img
+        File screenshot = new File("target/screenshots/component.png")
+
+        assertThat(screenshot, not(anExistingFile()))
+
+        takeScreenshot('component', image)
+
+        assertThat(screenshot, anExistingFile())
+    }
+}
---
+++ src/test/groovy/sc/tyro/provider/TyroWebTestExtension.groovy
@@ -0,0 +1,103 @@
+/**
+ * Copyright © 2020 Ovea (d.avenante@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sc.tyro.provider
+
+import io.github.bonigarcia.wdm.WebDriverManager
+import io.javalin.Javalin
+import org.junit.jupiter.api.extension.AfterAllCallback
+import org.junit.jupiter.api.extension.BeforeAllCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.openqa.selenium.Capabilities
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxOptions
+import org.testcontainers.containers.BrowserWebDriverContainer
+import sc.tyro.web.WebBundle
+
+import static java.lang.Boolean.valueOf
+import static java.lang.System.getenv
+import static java.net.InetAddress.getByName
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL
+
+/**
+ * @author David Avenante
+ * @since 1.0.0
+ */
+class TyroWebTestExtension implements BeforeAllCallback, AfterAllCallback {
+    private static Javalin app
+    public static WebDriver driver
+    private static BrowserWebDriverContainer container
+    public static String BASE_URL
+    private boolean isLocal = valueOf(getenv("local"))
+
+    @Override
+    void beforeAll(ExtensionContext extensionContext) throws Exception {
+        app = Javalin.create({
+            config -> config.addStaticFiles("/webapp")
+        }).start(0)
+
+        DatagramSocket socket = new DatagramSocket()
+        socket.connect(getByName("8.8.8.8"), 10002)
+        String host_ip = socket.localAddress.hostAddress
+        BASE_URL = "http://${host_ip}:${app.port()}/"
+
+        Capabilities options = capabilities()
+
+        if (isLocal) {
+            if (getenv("browser") == "firefox") {
+                WebDriverManager.firefoxdriver().setup()
+                driver = new FirefoxDriver(options)
+            } else {
+                WebDriverManager.chromedriver().setup()
+                driver = new ChromeDriver(options)
+            }
+        } else {
+            container = new BrowserWebDriverContainer()
+                    .withCapabilities(options)
+                    .withRecordingMode(RECORD_ALL, new File("./target/"))
+            container.start()
+
+            driver = container.webDriver
+        }
+
+        WebBundle.init(driver)
+    }
+
+    @Override
+    void afterAll(ExtensionContext extensionContext) throws Exception {
+        driver.close()
+        app.stop()
+        if (!isLocal) {
+            container.stop()
+        }
+    }
+
+    private capabilities() {
+        if (getenv("browser") == "firefox") {
+            Capabilities options = new FirefoxOptions()
+            if (!isLocal) options.setHeadless(true)
+            options.addArguments("--start-fullscreen")
+            options.addArguments("--start-maximized")
+            return options
+        } else {
+            Capabilities options = new ChromeOptions()
+            if (!isLocal) options.setHeadless(true)
+            return options
+        }
+    }
+}
